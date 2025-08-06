41f85fd00d4286eadffc24275f1e6279367af663
Felipe Texeira <109564897+DevWebFelipe@users.noreply.github.com>
2025-08-05 23:28:40 -0300

    Tipos de dados

---
+++ Nivel_5.JavaScript/Basico/VariaveisConstantes/Aula_03.js
@@ -0,0 +1,8 @@
+let userName = "Felipe"
+console.log(userName)
+console.log(typeof userName)
+
+console.log("String com aspas duplas")
+console.log("String com aspas simples")
+console.log("String com 'aspas' simples e duplas")
+console.log('String com "aspas" simples e duplas')
