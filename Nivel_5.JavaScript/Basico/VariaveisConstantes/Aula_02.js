524d3c0931856b8f44084bd7601bcc10b5d1722e
Felipe Texeira <109564897+DevWebFelipe@users.noreply.github.com>
2025-08-05 23:05:56 -0300

    variáveis e escopos

---
+++ Nivel_5.JavaScript/Basico/VariaveisConstantes/Aula_02.js
@@ -0,0 +1,16 @@
+console.log(user)
+var user = "Felipe"
+console.log(user)
+
+{
+  console.log(user)
+  var age = 31
+  let year = 2025
+}
+
+{
+  let year
+
+  console.log(age)
+  console.log(year)
+}
