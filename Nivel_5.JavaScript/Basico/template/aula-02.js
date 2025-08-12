0ca062cdf7461df3e73e29d98a2d77c247f1925b
Felipe Texeira <109564897+DevWebFelipe@users.noreply.github.com>
2025-08-11 22:56:57 -0300

    aula de seletores js 2

---
+++ Nivel_5.JavaScript/Basico/template/aula-02.js
@@ -0,0 +1,10 @@
+const guest = document.querySelector("#guest-2")
+console.log(guest)
+
+// Pega só o primeiro
+const guestByClass = document.querySelector(".guest")
+console.log(guestByClass)
+
+// Pega todos
+const guestsByClass = document.querySelectorAll(".guest")
+console.log(guestsByClass)
