2e53b216f0293140d3a73375681bcc21dd49003e
Felipe Texeira <109564897+DevWebFelipe@users.noreply.github.com>
2025-08-18 16:30:01 -0300

    renomado arquivos antigos e feito novas aulas

---
+++ Nivel_5.JavaScript/Basico/template/aulas/aula-06-Mipulando_atributos.js
@@ -0,0 +1,8 @@
+const input = document.querySelector("input")
+
+// atualizar um atributo - nome do atributo e valor
+//input.setAttribute("disabled", true)
+//input.setAttribute("type", "file")
+
+// remover um atributo
+//input.removeAttribute("id")
