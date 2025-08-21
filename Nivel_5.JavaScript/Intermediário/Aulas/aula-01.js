b7e64f7344d0d364e5ee776ae15dc7e2f1b409fc
Felipe Texeira <109564897+DevWebFelipe@users.noreply.github.com>
2025-08-21 20:25:53 -0300

    Aula 01

---
+++ Nivel_5.JavaScript/Intermediário/Aulas/aula-01.js
@@ -0,0 +1,38 @@
+// Criar um objeto vazio
+const obj = {}
+console.log(obj)
+console.log(typeof obj)
+
+console.log("")
+// Criar um objeto com propriedades e métodos
+const user = {
+  email: "felipe.flptxr@gmail.com",
+  age: 18,
+  // estrutura de aninhamento, um objeto dentro do outro
+  name: {
+    first_name: "Felipe",
+    surname: "Texeira",
+  },
+  address: {
+    city: "Gravatal",
+    street: "Rua geral",
+    number: 50,
+    postal_code: "89800-000",
+  },
+  messageArrow: () => {
+    console.log("Método criado com arrow function")
+  },
+  messageAnonimous: function () {
+    console.log("Método criado com função anonima")
+  },
+}
+
+console.log(user)
+console.log(typeof user)
+console.log("")
+user.messageAnonimous()
+user.messageArrow()
+console.log("")
+console.log("Nome:", user["name"]["first_name"] + " " + user.name.surname)
+console.log("E-mail:", user.email)
+console.log("Idade:", user["age"])
