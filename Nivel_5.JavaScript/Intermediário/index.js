c537386a5d6a506dfb0b620b10c1988444002509
Felipe Texeira <109564897+DevWebFelipe@users.noreply.github.com>
2025-08-24 00:04:27 -0300

    aula 10

--- Nivel_5.JavaScript/Intermediário/index.js
+++ Nivel_5.JavaScript/Intermediário/index.js
@@ -15,6 +15,7 @@ console.log(maskedEndNumber)
 
 console.log("")
 // aula 08
+
 let text = "Estudar, Aprender, Praticar"
 let separate = text.split(",")
 console.log(text)
@@ -23,3 +24,23 @@ console.log(separate[0])
 
 let joined = separate.join(" - ")
 console.log(joined)
+
+console.log("")
+// aula 09
+
+const message = "Estou estudando os fundamentos de javascript"
+
+// obter a posição inicial da palavra
+// se não econtrar, retorna -1
+// Tudo que foi visto, é extamente igual o Delphi, só muda a forma de escrever
+// ou seja, a lógica usada, será a mesma em qualquer linguagem, só a sintaxe
+// que muda, o mesmo vale para todo o resto que foi visto, objetos, classes,
+// tipos de dados, array etc... a lógica é a mesma
+// .slice = Copy
+// .split = SplitString
+// .padEnd e .padStart = (função interna StrZero por exe)
+// .indexOF = Pos
+// .includes = (Pos() > 0)
+// .joined = (função interna JuntarString)
+console.log(message.indexOf("estudando"))
+console.log(message.includes("fundamentos")) // True ou False
