f7d6f01fe683df57818841f75b7c3665502de9fa
Felipe Texeira <109564897+DevWebFelipe@users.noreply.github.com>
2025-08-25 23:45:19 -0300

    aula 10

---
+++ Nivel_5.JavaScript/Intermediário/Aulas/aula-10-While.js
@@ -0,0 +1,19 @@
+let execute = 1
+
+while (true) {
+  console.log(execute)
+
+  execute++
+
+  if (execute > 5) {
+    break
+  }
+}
+
+execute = 1
+
+while (execute <= 5) {
+  console.log(execute)
+
+  execute++
+}
