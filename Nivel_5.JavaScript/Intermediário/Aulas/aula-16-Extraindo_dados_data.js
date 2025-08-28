b5a0af2c670af225c8afdd2e44a3ffa506bf8858
Felipe Texeira <109564897+DevWebFelipe@users.noreply.github.com>
2025-08-28 19:19:14 -0300

    aula 16

---
+++ Nivel_5.JavaScript/Intermediário/Aulas/aula-16-Extraindo_dados_data.js
@@ -0,0 +1,38 @@
+let date = new Date("2022-07-07T10:30:15")
+
+console.log(date)
+
+console.log("")
+
+// Dia da semana (0 - 6) (Domingo - Sábado)
+console.log(date.getDay())
+
+console.log("")
+
+// Dia do mes (0 - 30) (1 - 31)
+console.log(date.getDate())
+
+console.log("")
+
+// Mes do ano (0 - 11) (Janeiro - Dezembro)
+console.log(date.getMonth())
+
+console.log("")
+
+// Ano
+console.log(date.getFullYear())
+
+console.log("")
+
+// horas
+console.log(date.getHours())
+
+console.log("")
+
+// minutos
+console.log(date.getMinutes())
+
+console.log("")
+
+// segundos
+console.log(date.getSeconds())
---
+++ Nivel_5.JavaScript/Intermediário/Aulas/aula-17-Modificando_data_hora.js
@@ -0,0 +1,41 @@
+let date = new Date("July 7, 2022 10:30:15")
+
+console.log(date)
+
+console.log("")
+
+// modificar o ano
+date.setFullYear(2030)
+console.log(date)
+
+console.log("")
+
+// modificar o mes
+date.setMonth(9)
+console.log(date)
+
+console.log("")
+
+// modificar o dia
+date.setDate(23)
+console.log(date)
+
+console.log("")
+
+// modificar a hora
+date.setHours(11)
+console.log(date)
+
+console.log("")
+
+// modificar o minuto
+date.setMinutes(3)
+console.log(date)
+
+console.log("")
+
+// modificar o ano
+date.setSeconds(35)
+console.log(date)
+
+console.log("")
