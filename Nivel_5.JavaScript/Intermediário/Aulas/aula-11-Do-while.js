ff9b5b086916e87e15efb5dea0c6bd49abe83e4d
Felipe Texeira <109564897+DevWebFelipe@users.noreply.github.com>
2025-08-27 21:13:32 -0300

    aula 11

---
+++ Nivel_5.JavaScript/Intermediário/Aulas/aula-11-Do-while.js
@@ -0,0 +1,10 @@
+let value = 0
+
+do {
+  value++
+  console.log(value)
+} while (value < 10)
+// primeira executa, depois verifica
+// diferente do while () {} que primeiro valida, depois executa
+
+console.log("Finalizou o laço")
