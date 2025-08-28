5a3d6aad08693cf8aa6d64bf0a290e8abf7021c4
Felipe Texeira <109564897+DevWebFelipe@users.noreply.github.com>
2025-08-27 21:14:02 -0300

    aula 12

---
+++ Nivel_5.JavaScript/Intermediário/Aulas/aula-12-for.js
@@ -0,0 +1,26 @@
+// no delphi for mI = 0 to 10 do
+for (step = 0; step < 10; step++) {
+  console.log(step)
+}
+
+// exemplo com tabuada
+console.log("")
+console.log("### tabuada ###")
+console.log(`7x0 = ${7 * 0}`)
+console.log(`7x1 = ${7 * 1}`)
+console.log(`7x2 = ${7 * 2}`)
+console.log(`7x3 = ${7 * 3}`)
+console.log(`7x4 = ${7 * 4}`)
+console.log(`7x5 = ${7 * 5}`)
+console.log(`7x6 = ${7 * 6}`)
+console.log(`7x7 = ${7 * 7}`)
+console.log(`7x8 = ${7 * 8}`)
+console.log(`7x9 = ${7 * 9}`)
+console.log(`7x10 = ${7 * 10}`)
+
+// exemplo de tabuada com for
+console.log("")
+console.log("### com for ###")
+for (step = 0; step <= 10; step++) {
+  console.log(`7x${step} = ${7 * step}`)
+}
